f79fabb963e154be799c755603951f77ab9ebb7b
Lourence Epino <lepino17gdev@gmail.com>
2025-10-19 22:48:15 +0800

    updates

--- appointments/static/js/sidebar_appointments.js
+++
@@ -1,8 +0,0 @@
-$(".sidebar").on("click", "#appointments-dropdown", function () { 
-  const sidebar = $(".sidebar");
-  console.log(sidebar);
-  if (sidebar.length && !sidebar.hasClass("expanded")) {
-    console.log("Expanding sidebar")
-    $("#toggleSidebar").click(); // Expand sidebar if collapsed
-  }
-})
